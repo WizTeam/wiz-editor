8bc95b87e67bdea1f3505f511b2e64e613d92feb
steve.wei <wsj@wiz.cn>
2020-12-11 12:21:14 +0800

    react demo

---
+++ react-demo/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import './App.css';
+import * as WizEditor from 'wiz-editor/client';
+
+function App() {
+
+  const editorRef = useRef();
+
+  useEffect(() => {
+    if (!editorRef.current || editorRef.current.editor) {
+      return;
+    }
+
+    console.log('-------------------------------- init Editor');
+
+    const WsServerUrl = `ws://${window.location.host.replace('3000', '9000')}`;
+    const docId = '_e9kD3NTs';
+    const user = {
+      userId: `${new Date().valueOf()}`,
+      displayName: 'Hunter',
+    };
+    const options = {
+      // lang: LANGS.ZH_CN,
+      serverUrl: WsServerUrl,
+      user,
+    };
+    editorRef.current.editor = WizEditor.createEditor(editorRef.current, options, docId);
+  }, []);
+  
+  return (
+    <div className="App">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        textAlign: 'initial',
+      }}>
+        <div id="editor" ref={editorRef} style={{
+            width: '100%',
+            maxWidth: 800,
+            border: 'solid 1px #ccc',
+            margin: 8,
+            overflow: 'auto',
+          }}>
+          Editor001
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
