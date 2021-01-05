f8d8194ee5acea872a144387f32a8760997e3f12
steve.wei <wsj@wiz.cn>
2021-01-05 11:35:47 +0800

    add custom complex block examples

--- h5/src/index.ts
+++ h5/src/index.ts
@@ -31,6 +31,18 @@ import {
   EmbedElement,
   EMBED_TYPE,
   embedUtils,
+  RichTextDocument,
+  BlockData,
+  BlockContentElement,
+  ContainerElement,
+  containerUtils,
+  BlockOptions,
+  EditorDoc,
+  DocBlock,
+  BlockCommand,
+  CommandParams,
+  ContainerData,
+  Block,
 } from 'wiz-editor/client';
 import { AuthMessage, AuthPermission } from 'wiz-editor/commons/auth-message';
 
@@ -42,77 +54,282 @@ let currentEditor: Editor | null;
 
 // -------------------custom embed block----------------
 
-interface EmbedButtonsData extends EmbedData {
-  count?: number;
-}
-
-function handleButtonClick(event: Event) {
-  const button = event.target as HTMLButtonElement;
-  alert(`you clicked button ${button.innerText}`);
-};
-//
-
-function createElement(editor: Editor, data: EmbedData): EmbedElement {
-  assert(data);
-  const div = document.createElement('div');
-  div.style.display = 'inline';
-  const child = document.createElement('div');
-  div.appendChild(child);
-  //
-  const buttonsData = data as EmbedButtonsData;
-  const count = buttonsData.count || 10;
-  //
-  div.setAttribute('data-count', `${count}`);
-  //
-  for (let i = 0; i < count; i++) {
-    const button = document.createElement('button');
-    button.innerText = `button-${i}`;
-    button.onclick = handleButtonClick;
-    child.appendChild(button);
+(() => {
+  interface EmbedButtonsData extends EmbedData {
+    count?: number;
   }
-  //
-  return div as unknown as EmbedElement;
-}
 
-function saveData(editor: Editor, embed: EmbedElement): EmbedData {
-  assert(embed instanceof HTMLDivElement);
-  const count = Number.parseInt(embed.getAttribute('data-count') || '10', 10);
-  return {
-    count,
+  function handleButtonClick(event: Event) {
+    const button = event.target as HTMLButtonElement;
+    alert(`you clicked button ${button.innerText}`);
   };
-}
+  //
 
-function updateData(editor: Editor, embed: EmbedElement, data: EmbedData): void {
-  assert(embed instanceof HTMLHRElement);
-  assert(data);
-  //
-  assert(embed.children.length === 1);
-  const child = embed.children[0];
-  child.innerHTML = '';
-  //
-  const buttonsData = data;
-  const count = buttonsData.count || 10;
-  //
-  for (let i = 0; i < count; i++) {
-    const button = document.createElement('button');
-    button.innerText = `button-${i}`;
-    button.onclick = handleButtonClick;
-    child.appendChild(button);
+  function createElement(editor: Editor, data: EmbedData): EmbedElement {
+    assert(data);
+    const div = document.createElement('div');
+    const child = document.createElement('div');
+    div.appendChild(child);
+    //
+    const buttonsData = data as EmbedButtonsData;
+    const count = buttonsData.count || 10;
+    //
+    div.setAttribute('data-count', `${count}`);
+    //
+    for (let i = 0; i < count; i++) {
+      const button = document.createElement('button');
+      button.innerText = `button-${i}`;
+      button.onclick = handleButtonClick;
+      child.appendChild(button);
+    }
+    //
+    return div as unknown as EmbedElement;
   }
-}
 
-const buttonsEmbed = {
-  createElement,
-  saveData,
-  updateData,
-};
+  function saveData(editor: Editor, embed: EmbedElement): EmbedData {
+    assert(embed instanceof HTMLDivElement);
+    const count = Number.parseInt(embed.getAttribute('data-count') || '10', 10);
+    return {
+      count,
+    };
+  }
 
-embedUtils.registerEmbed('buttons' as EMBED_TYPE, buttonsEmbed);
+  function updateData(editor: Editor, embed: EmbedElement, data: EmbedData): void {
+    assert(embed instanceof HTMLHRElement);
+    assert(data);
+    //
+    assert(embed.children.length === 1);
+    const child = embed.children[0];
+    child.innerHTML = '';
+    //
+    const buttonsData = data;
+    const count = buttonsData.count || 10;
+    //
+    for (let i = 0; i < count; i++) {
+      const button = document.createElement('button');
+      button.innerText = `button-${i}`;
+      button.onclick = handleButtonClick;
+      child.appendChild(button);
+    }
+  }
 
+  const buttonsEmbed = {
+    createElement,
+    saveData,
+    updateData,
+  };
+
+  embedUtils.registerEmbed('buttons' as EMBED_TYPE, buttonsEmbed);
+})();
+
+const TEST_BLOCK_TYPE = 'test';
+// ------------------ create a custom complex block -------
+(() => {
+  interface TestComplexBlockTemplateData {
+    imgSrc: string;
+  };
+
+  interface TestComplexBlockData extends TestComplexBlockTemplateData, BlockData {
+  }
+
+  function createBlockTemplateData(editor: Editor, options: TestComplexBlockTemplateData) {
+    //
+    const blocks = [
+      blockUtils.createBlockData(editor, BLOCK_TYPE.TEXT, {
+        text: new RichTextDocument([]),
+      }),
+    ];
+    //
+    const containerId = editor.createEmptyChildContainerData(blocks, genId());
+    const children = [containerId];
+    //
+    return {
+      children,
+      ...options,
+    };
+  }
+
+  function createBlockContent(editor: Editor, id: string, data: BlockData): BlockContentElement {
+    //
+    assert(data);
+    const blockData = data as TestComplexBlockData;
+    const blockContent = document.createElement('div') as unknown as BlockContentElement;
+    blockContent.style.border = '1px solid';
+    blockContent.style.display = 'flex';
+    blockContent.style.alignItems = 'end';
+    const img = document.createElement('img');
+    img.src = blockData.imgSrc;
+    blockContent.appendChild(img);
+    //
+    assert(blockData.children);
+    assert(blockData.children.length === 1);
+    const subContainerId = blockData.children[0];
+    const containerBlocks = editor.getChildContainerData(subContainerId);
+    const container = editor.createChildContainer(blockContent, subContainerId, containerBlocks);
+    assert(container);
+    return blockContent;
+  }
+
+  function getChildImage(block: BlockElement): HTMLImageElement {
+    assert(blockUtils.isBlock(block));
+    assert(blockUtils.getBlockType(block) === (TEST_BLOCK_TYPE as any));
+    const content = blockUtils.getBlockContent(block);
+    assert(content.children.length === 2);
+    assert(content.children[0] instanceof HTMLImageElement);
+    return content.children[0];
+  }
+
+  function getChildContainer(block: BlockElement): ContainerElement {
+    assert(blockUtils.isBlock(block));
+    assert(blockUtils.getBlockType(block) === (TEST_BLOCK_TYPE as any));
+    const content = blockUtils.getBlockContent(block);
+    assert(content.children.length === 2);
+    const container = content.children[1] as ContainerElement;
+    return container;
+  }
+
+  function saveData(block: BlockElement): BlockData {
+    assert(block);
+    //
+    const subContainer = getChildContainer(block);
+    const subContainerId = containerUtils.getContainerId(subContainer);
+    const children = [subContainerId];
+    const id = blockUtils.getBlockId(block);
+    const image = getChildImage(block);
+    //
+    const blockData: TestComplexBlockData = {
+      id,
+      type: TEST_BLOCK_TYPE as any,
+      text: new RichTextDocument([]),
+      children,
+      imgSrc: image.src,
+    };
+    return blockData;
+  }
+
+  function updateBlockData(block: BlockElement, data: BlockData) {
+    //
+    const newData = data as TestComplexBlockData;
+    //
+    const oldData = saveData(block) as TestComplexBlockData;
+    assert(oldData.children);
+    assert(newData.children);
+    assert(oldData.children[0] === newData.children[0]);
+    //
+    if (oldData.imgSrc !== newData.imgSrc) {
+      //
+      const image = getChildImage(block);
+      image.src = newData.imgSrc;
+    }
+  }
+
+  function getChildContainersData(block: BlockElement): ContainerData[] {
+    //
+    const content = getChildContainer(block);
+    const containerId = containerUtils.getContainerId(content);
+    const blocks: BlockData[] = [];
+    containerUtils.getAllBlocks(content).forEach((childBlock) => {
+      const blockData = blockUtils.saveData(childBlock);
+      blocks.push(blockData);
+    });
+    return [{
+      id: containerId,
+      blocks,
+    }];
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function getCaretPos(block: BlockElement, node: Node, nodeOffset: number): number {
+    const container = getChildContainer(block);
+    if (node === container) {
+      assert(nodeOffset === 0 || nodeOffset === 1);
+      return nodeOffset;
+    }
+    // sub blocked has been deleted, ignore
+    return 0;
+  }
+
+  function createRange(block: BlockElement, pos: number): Range {
+    assert(block);
+    assert(pos === 0 || pos === -1 || pos === 1);
+    //
+    const container = getChildContainer(block);
+    assert(container);
+    const blocks = containerUtils.getAllBlocks(container);
+    const childBlock = pos === 0 ? blocks[0] : blocks[blocks.length - 1];
+    assert(childBlock);
+    const offset = pos === 0 ? 0 : -1;
+    const ret = blockUtils.createRange(childBlock, offset);
+    return ret;
+  }
+
+  function getSubContainerInComplexBlock(block: BlockElement,
+    element: HTMLElement, type: 'top' | 'right' | 'bottom' | 'left') {
+    //
+    assert(type);
+    return null;
+  }
+
+  function getBlockOptions(): BlockOptions {
+    return {
+      textBlock: false,
+      complexBlock: true,
+    };
+  }
+
+  function replaceChildrenId(editorDoc: EditorDoc, blockData: DocBlock): void {
+    const doc = editorDoc;
+    const children = blockData.children;
+    assert(children);
+    const oldId = children[0];
+    assert(oldId);
+    const newId = genId();
+    doc[newId] = doc[oldId];
+    delete doc[oldId];
+    // eslint-disable-next-line no-param-reassign
+    blockData.children = [newId];
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function executeBlockCommand(block: BlockElement, command: BlockCommand, params?: CommandParams): any {
+    assert(blockUtils.getBlockType(block) === (TEST_BLOCK_TYPE as any));
+    //
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function handleBlockLoaded(block: BlockElement) {
+    //
+  }
+
+  function accept(type: BOX_TYPE | BLOCK_TYPE): boolean {
+    assert(type);
+    // accept all non-complex blocks
+    return true;
+  }
+
+  const TestComplex: Block = {
+    getBlockOptions,
+    createBlockTemplateData,
+    createBlockContent,
+    updateBlockData,
+    saveData,
+    getChildContainersData,
+    getCaretPos,
+    createRange,
+    getSubContainerInComplexBlock,
+    replaceChildrenId,
+    executeBlockCommand,
+    handleBlockLoaded,
+    accept,
+  };
+
+  blockUtils.registerBlockType(TEST_BLOCK_TYPE as any, TestComplex);
+})();
+
+const CALENDAR_IMAGE_URL = 'https://www.wiz.cn/wp-content/new-uploads/b75725f0-4008-11eb-8f21-01eb48012b63.jpeg';
 
 // -------------------create a custom calendar item----------
 (() => {
-  const CALENDAR_IMAGE_URL = 'https://www.wiz.cn/wp-content/new-uploads/b75725f0-4008-11eb-8f21-01eb48012b63.jpeg';
   const CALENDAR_BOX_TYPE = 'calendar';
 
   interface CalendarBoxData extends BoxData {
@@ -840,6 +1057,7 @@ function handleCommandStatusChanged(status: CommandStatus): void {
   (document.getElementById('office-button') as HTMLButtonElement).disabled = disabledInsertBlock;
   (document.getElementById('math-block') as HTMLButtonElement).disabled = disabledInsertBlock;
   (document.getElementById('buttons') as HTMLButtonElement).disabled = disabledInsertBlock;
+  (document.getElementById('complex-block') as HTMLButtonElement).disabled = disabledInsertBlock;
 
   (document.getElementById('math') as HTMLButtonElement).disabled = disabledInsertBox;
   (document.getElementById('project') as HTMLButtonElement).disabled = disabledInsertBox;
@@ -1030,9 +1248,21 @@ buttons.forEach((button) => {
 
 document.getElementById('buttons')?.addEventListener('click', () => {
   assert(currentEditor);
-  const count = Date.now() % 5 + 5;
+  const count = (Date.now() % 5) + 5;
   currentEditor.insertEmbed(null, -2, 'buttons' as any, {
     count,
+  });
+});
+
+document.getElementById('complex-block')?.addEventListener('click', () => {
+  assert(currentEditor);
+  const blockData = blockUtils.createBlockData(currentEditor, TEST_BLOCK_TYPE as any, {
+    imgSrc: CALENDAR_IMAGE_URL,
+  });
+  currentEditor.insertBlock(null, -2, 'test' as any, blockData, {
+    fromUndo: false,
+    focusToBlock: true,
+    localAction: true,
   });
 });
 
